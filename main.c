7aee8ec08b521d9b5e66e018f60a4e4aeb22e35d
Gudrat99 <157212873+Gudrat99@users.noreply.github.com>
2024-01-20 15:41:39 +0400

    Update main.c

--- main.c
+++ main.c
@@ -1,9 +1,9 @@
 int main()
 {
   int i;
-  for (i = 0; i < 32; i++)
+  for (i = 0; i < 32; i++)  // changed the loop limit to 32
   {
-    printf("Hello School");
+    printf("Hello School\n");  // changed the string to "Hello School"
   }
   return 0;
 }
